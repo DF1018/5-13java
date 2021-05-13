a2e0deab5f2af02d9f0f5d9dc24bfa4ca7e13e29
DF1018 <71478187+DF1018@users.noreply.github.com>
2021-05-13 15:05:06 +0800

    Update and rename README.md to README.cpp

--- README.cpp
+++ README.cpp
@@ -1,9 +1,9 @@
 # 5-13java
 ![image](https://github.com/DF1018/5-13java/blob/main/%E6%93%B7%E5%8F%96.PNG?raw=true)
+
 public extendDemo;
 
 class CMath {
-
    public void getMax(int a, int b) {
       int bigNum;
       if (a > b)
